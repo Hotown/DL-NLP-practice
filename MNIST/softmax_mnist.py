6a274556a7d27c8ba9c3ae41ba41a661f3c3a228
Hotown <hotown0515@163.com>
2017-12-07 00:04:08 +0800

    :exclamation: change the init function of W and fix bug in cross entropy

--- MNIST/softmax_mnist.py
+++ MNIST/softmax_mnist.py
@@ -2,15 +2,14 @@ import tensorflow as tf
 
 # 测试数据导入
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-
 
 # 模型建立
 x = tf.placeholder(tf.float32, [None, 784])
 
 # 权值矩阵和偏置数
-W = tf.Variable(tf.zeros([784, 10]))
+W = tf.Variable(tf.random_normal([784, 10], stddev=0.35))
 b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
@@ -19,7 +18,7 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder("float", [None, 10])
 
 # 计算交叉熵作为loss function = - ∑y'log(y)
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+cross_entropy = -tf.reduce_mean(tf.reduce_sum(y_ * tf.log(y)))
 
 # 梯度下降法，学习率0.01，得到最小交叉熵
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
@@ -30,13 +29,11 @@ with tf.Session() as sess:
     sess.run(init)
 
     # 模型训练
-    for i in range(1000):
+    for i in range(5000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
         if i % 100 == 0:
             print(i, sess.run(W).flatten(), sess.run(b).flatten())
-
-
 
     # 比较预测值和真实值
     # return a array of boolean
